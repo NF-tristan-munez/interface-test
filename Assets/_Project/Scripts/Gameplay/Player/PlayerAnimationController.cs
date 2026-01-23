89ffb9fc391e0829107f8656218587e929a524b5
tristan-dev <dunkwad@gmail.com>
2026-01-23 14:58:05 +0800

    Interface Activity 3: Add animations for Player rig

---
+++ Assets/_Project/Scripts/Gameplay/Player/PlayerAnimationController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerAnimationController : SingletonPersistent<PlayerAnimationController>
+{
+    private void Awake()
+    {
+        //Initialize mono extension
+        Initialize();
+    }
+    private void Start()
+    {
+        //Events
+        OnSubscriptionSet();
+    }
+    
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+    
+    public override void OnSubscriptionSet()
+    {
+        base.OnSubscriptionSet();
+    }    
+}
