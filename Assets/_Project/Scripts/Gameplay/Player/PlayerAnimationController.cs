b21253d0280aa90858fd0dd05695c0859de4ba33
tristan-dev <dunkwad@gmail.com>
2026-01-23 15:12:43 +0800

    Interface Activity 3: Import state machine scripts

--- Assets/_Project/Scripts/Gameplay/Player/PlayerAnimationController.cs
+++ Assets/_Project/Scripts/Gameplay/Player/PlayerAnimationController.cs
@@ -1,25 +1,7 @@
+using NF.Main.Core;
 using UnityEngine;
 
 public class PlayerAnimationController : SingletonPersistent<PlayerAnimationController>
 {
-    private void Awake()
-    {
-        //Initialize mono extension
-        Initialize();
-    }
-    private void Start()
-    {
-        //Events
-        OnSubscriptionSet();
-    }
     
-    public override void Initialize()
-    {
-        base.Initialize();
-    }
-    
-    public override void OnSubscriptionSet()
-    {
-        base.OnSubscriptionSet();
-    }    
 }
